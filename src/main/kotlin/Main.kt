e6a11ebaa380523c2fc9377ff7980446ad7de5e4
Isaac Santiago <eoisaacc@gmail.com>
2023-09-02 18:42:12 -0300

    feat: get initial player

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 typealias GameBoard = MutableList<String>
+typealias Players = List<String>
 
 fun printBoard(cells: GameBoard) {
     val board = String.format(
@@ -10,14 +11,29 @@ fun printBoard(cells: GameBoard) {
          %s | %s | %s 
         """.trimIndent(), *Array(9) { cells[it] }
     )
-    print(board)
+    println(board)
 }
 
+fun getPlayer(players: Players): String {
+    val possiblePlayers = players.joinToString(" or ")
+    println("Who will start playing $possiblePlayers?")
 
+    while (true) {
+        print("=> ")
+        val input = readln().uppercase()
 
+        if (input in players) {
+            return input
+        }
 
+        println("Invalid player, try again")
+    }
+}
 
 fun main() {
+    val players = listOf("X", "O")
     val boardCells: GameBoard = mutableListOf("0", "1", "2", "3", "4", "5", "6", "7", "8")
-    printBoard(boardCells)
+
+    var currentPlayer = getPlayer(players)
+
 }
