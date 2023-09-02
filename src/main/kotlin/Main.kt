c5d0f7c952e1e00fd271b2cd79eaf5c8dc893b47
Isaac Santiago <eoisaacc@gmail.com>
2023-09-02 18:52:12 -0300

    feat: get and validate current player move

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,6 +14,12 @@ fun printBoard(cells: GameBoard) {
     println(board)
 }
 
+
+fun clearConsole() {
+    Array(30) { print("\n") }
+}
+
+
 fun getPlayer(players: Players): String {
     val possiblePlayers = players.joinToString(" or ")
     println("Who will start playing $possiblePlayers?")
@@ -30,10 +36,43 @@ fun getPlayer(players: Players): String {
     }
 }
 
+
+fun getPlayerMove(player: String, players: Players, boardCells: GameBoard): Int {
+    println("Player $player, select a board cell")
+
+    while (true) {
+        print("=> ")
+        val input = readln().toIntOrNull()
+
+        if (input == null || input !in 0..8) {
+            println("Invalid input, try again!")
+            continue
+        }
+
+        if (boardCells[input] in players) {
+            println("Cell already taken, player $player, try again!")
+            continue
+        }
+
+        return input
+    }
+}
+
+
 fun main() {
     val players = listOf("X", "O")
     val boardCells: GameBoard = mutableListOf("0", "1", "2", "3", "4", "5", "6", "7", "8")
 
     var currentPlayer = getPlayer(players)
+
+    while (true) {
+        clearConsole()
+        printBoard(boardCells)
+
+        val playerMove = getPlayerMove(currentPlayer, players, boardCells)
+        boardCells[playerMove] = currentPlayer
+
+        currentPlayer = if (currentPlayer == players[0]) players[1] else players[0]
+    }
 
 }
