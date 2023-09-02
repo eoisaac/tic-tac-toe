5f7a60bf402cc0a1dc7be154a630dd04c55cc3dd
Isaac Santiago <eoisaacc@gmail.com>
2023-09-02 18:14:13 -0300

    refactor: replace matrix board by list board

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,23 @@
-typealias TicTacToeBoard = List<List<String>>
+typealias GameBoard = MutableList<String>
 
-fun printBoard(board: TicTacToeBoard) {
-    board.mapIndexed { rowIndex, row ->
-        row.mapIndexed { columnIndex, column ->
-            print(if (columnIndex % 2 != 0)  "| $column |" else " $column ")
-        }
-        if (rowIndex < board.size - 1) println("\n---+---+---")
-    }
+fun printBoard(cells: GameBoard) {
+    val board = String.format(
+        """
+         %s | %s | %s 
+        ---+---+---
+         %s | %s | %s 
+        ---+---+---
+         %s | %s | %s 
+        """.trimIndent(), *Array(9) { cells[it] }
+    )
+    print(board)
 }
 
-fun welcome() {
-    println("Welcome to tic tac toe!")
 
-}
 
 
 
 fun main() {
-    val board: TicTacToeBoard = listOf(
-        listOf("A", "B", "C"),
-        listOf("D", "E", "F"),
-        listOf("G", "H", "I"),
-    )
-
-    printBoard(board)
+    val boardCells: GameBoard = mutableListOf("0", "1", "2", "3", "4", "5", "6", "7", "8")
+    printBoard(boardCells)
 }
